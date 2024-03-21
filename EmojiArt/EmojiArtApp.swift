30a37a89b23f2a153da66902ae2528d7737cce56
sarahchang2013 <sarah_sarah99@hotmail.com>
2024-03-21 10:20:41 +0800

    Comment on multiple windows sharing one view model WindowGroup defaultDocument

--- EmojiArt/EmojiArtApp.swift
+++ EmojiArt/EmojiArtApp.swift
@@ -12,6 +12,8 @@ struct EmojiArtApp: App {
     @StateObject var defaultDocument = EmojiArtDocument()
     @StateObject var paletteStore = PaletteStore(named: "Main Palettes")
     var body: some Scene {
+        //This shares a view model across views, multiple windows show the same document,
+        // but pan and zoom are different for each view
         WindowGroup {
             EmojiArtDocumentView(document: defaultDocument)
                 .environmentObject(paletteStore)
